e4f56b23ee84685e9fb6fd4834db2aa751ca1684
imvm <imvm@cin.ufpe.br>
2019-12-25 09:47:59 -0300

    Refactor reading list to read from JSON

---
+++ content/blog/bash-design-patterns/index.md
@@ -0,0 +1,7 @@
+---
+title: Playing with OOP Design Patterns in Bash
+date: "2019-08-31"
+description: Exploring Builder, Command and Object Pool pattern implementations in Bash.
+---
+
+WIP, Sorry!
--- src/pages/reading.js
+++ src/pages/reading.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { graphql } from "gatsby"
+import JSONData from "../../content/books.json"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -20,26 +21,36 @@ class ReadingPage extends React.Component {
             this list as a record of my reading activity.</p>
         <h3>Top 3</h3>
         <ul>
-            <li>Johnny Got His Gun by Dalton Trumbo.</li>
-            <li>Sirens of Titan by Kurt Vonnegut</li>
-            <li>The Silmarillion by J.R.R Tolkien</li>
+          {JSONData.sort(function(a, b) {
+            return b.score - a.score;
+          }).slice(0, 3).map((data) => {
+            return <li>{data.title} by {data.authors.join(", ")}</li>
+          })}
         </ul>
         <h3>Currently reading</h3>
         <ul>
-            <li>A carta: Para entender a constituição brasileira by Naercio Menezes Filho and André Portela Souza.</li>
-            <li>The Rise and Fall of the Dinosaurs: A New History of a Lost World by Steve Brussate</li>
-            <li>Push Notifications by Tutorials by the Scott Grosch and the raywenderlich Tutorial Team.</li>
+          {JSONData.filter((data) => {
+            return data.reading_status === "reading";
+          }).map((data) => {
+            return <li>{data.title} by {data.authors.join(", ")}</li>
+          })}
         </ul>
         <h3>On my backlog</h3>
         <ul>
-            <li>The Castle by Franz Kafka</li>
-            <li>Bad Blood by John Carreyou.</li>
-            <li>Catch and Kill by Ronan Farrow.</li>
+          {JSONData.filter((data) => {
+            return data.reading_status === "unread";
+          }).map((data) => {
+            return <li>{data.title} by {data.authors.join(", ")}</li>
+          })}
         </ul>
         <h3>Recently read</h3>
         <ul>
-            <li>Weapons of Math Destruction by Cathy O'Neil</li>
-            <li>Microcopy: The Complete Guide by Kinneret Yifrah.</li>
+          {JSONData.filter((data) => {
+            return data.reading_status === "finished"
+             && data.recently_read === true;
+          }).map((data) => {
+            return <li>{data.title} by {data.authors.join(", ")}</li>
+          })}
         </ul>
       </Layout>
     )
